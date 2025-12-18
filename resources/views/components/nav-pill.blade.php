e8e82c40a5f0152a8a94e25c3f8785988a9f3446
Nick Dillon <nickhds@gmail.com>
2025-12-18 15:42:02 -0600

    style: update light mode hover

--- resources/views/components/nav-pill.blade.php
+++ resources/views/components/nav-pill.blade.php
@@ -4,7 +4,7 @@
     <div
         @class([
             'bg-amber-400! text-neutral-800!' => $current,
-            'bg-neutral-100 dark:bg-neutral-800 text-neutral-600 dark:text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-100 hover:bg-neutral-200/75 hover:bg-neutral-700/75 px-3 py-1 text-sm font-medium text-center rounded-full items-center'
+            'bg-neutral-100 dark:bg-neutral-800 text-neutral-600 dark:text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-100 hover:bg-neutral-200/75 hover:bg-neutral-300/50 dark:hover:bg-neutral-700/75 px-3 py-1 text-sm font-medium text-center rounded-full items-center'
         ])
     >
         {{ $slot }}
